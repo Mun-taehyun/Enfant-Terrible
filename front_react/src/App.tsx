d6429fec07c76b8464f8ec07dbdf327ff33a93d5
seunggi <she361@naver.com>
2026-01-20 15:53:25 +0900

    Restore main with original files

---
+++ front_react/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+
+
+//라우팅 
+function App() {
+  
+  //UserContainer에 사용자를 구현하고 
+  //AdminContainer에 관리자를 구현하겠습니다.
+  return ( 
+    <Routes>
+      <Route element={<UserContainer/>}>
+          
+      </Route>
+      <Route element={<AdminContainer/>}>
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
