1ae180b91066d63b19e1c64f2d922ff9f1466f38
tvok29 <wltnxo@gmail.com>
2026-01-26 10:19:13 +0900

    머지

--- back_springboot/src/main/java/com/enfantTerrible/enfantTerrible/EnfantTerribleApplication.java
+++ back_springboot/src/main/java/com/enfantTerrible/enfantTerrible/EnfantTerribleApplication.java
@@ -1,17 +1,11 @@
 package com.enfantTerrible.enfantTerrible;
 
-<<<<<<< HEAD
-=======
 import org.springframework.scheduling.annotation.EnableScheduling;
->>>>>>> springboot-clean
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-<<<<<<< HEAD
-=======
 @EnableScheduling
->>>>>>> springboot-clean
 public class EnfantTerribleApplication {
 
 	public static void main(String[] args) {
